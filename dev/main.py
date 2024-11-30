d8847b73587fe3825fe898390cde20b0b992c5f4
HikinoKota <koutah130101@gmail.com>
2024-11-30 10:06:46 +0900

    #20 アブストをさらに細かく分け、論文情報を辞書形式で返す関数を作成

--- dev/main.py
+++ dev/main.py
@@ -1,8 +1,9 @@
+import json
 from logging import getLogger, StreamHandler, INFO
 import xml.etree.ElementTree as ET
 
 from Bio import Entrez
-import deepl
+from openai import OpenAI
 from openpyxl import Workbook
 from openpyxl.styles import Alignment
 
@@ -19,16 +20,16 @@ logger.addHandler(s_handler)
 
 def main():
     # キーワードからPMIDを取得
-    pmids = search_pmids(keyword, max_results=10)
+    pmids = search_pmids(keyword, max_results=2)
 
     # 論文情報をまとめて取得
     articles = fetch_articles(pmids)
 
-    # # ワークブックを作成
-    # wb = Workbook()
-    # ws = wb.active
-    # header = ["PMID", "Title", "Journal", "PubYear", "Abst_en"]
-    # ws.append(header)
+    # ワークブックを作成
+    wb = Workbook()
+    ws = wb.active
+    header = ["PMID", "Title", "Journal", "PubYear", "Objective", "Participants", "Exposure", "Comparison", "Outcome", "Results"]
+    ws.append(header)
 
     # # データをExcelに書き込む
     # for article in articles:
@@ -80,23 +81,62 @@ def fetch_articles(pmids: list[str]) -> list[dict]:
 
     articles = []
     for article in root.findall(".//PubmedArticle"):
+        pmid = article.findtext(".//PMID")
         title = article.findtext(".//ArticleTitle")
         journal = article.findtext(".//Journal/Title")
         pub_year = article.findtext(".//PubDate/Year")
+        summary_dic = {"PMID": pmid, "Title": title, "Journal": journal, "PubYear": pub_year}
         abstract = "".join([abst.text for abst in article.findall(".//AbstractText")])
-        pmid = article.findtext(".//PMID", default="")
-        articles.append({
-            "pmid": pmid,
-            "title": title,
-            "journal": journal,
-            "pub_year": pub_year,
-            "abstract": abstract
-        })
+        # 辞書形式でさらに要約されたアブストを結合    
+        summary_dic |= summarise_abst(abstract)
+        
+        articles.append(summary_dic)
+        
     logger.info("論文情報の取得を完了")
         
     return articles
 
+def summarise_abst(abst: str) -> dict[str, str]:
+    """アブストから目的や結果などを要約する関数
 
+    Args:
+        abst (str): 英文アブスト
+
+    Returns:
+        dict[str, str]: キーに項目名、バリューに内容
+    """
+    
+    client = OpenAI()
+    prompt = """
+    あなたは{# 役割}です。{# 形式}にしたがって{# 入力文}を要約してください。
+
+    # 役割
+    英語と日本語が得意な臨床研究の専門家
+
+    # 形式
+    ・JSON形式
+    ・キーは絶対に「"Objective", "Participants", "Exposure", "Comparison", "Outcome", "Results"」
+    ・バリューは各キーに該当する内容を{# 入力文}から抽出し、日本語で100字以内に要約する
+    
+    # 入力文
+    """
+    prompt += abst
+    
+
+    completion = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    # 先頭の```jsonと最後の```を取り除く
+    res = completion.choices[0].message.content
+    json_str = res[7:-3]
+    return json.loads(json_str)
 
 if __name__ == "__main__":
     main()
